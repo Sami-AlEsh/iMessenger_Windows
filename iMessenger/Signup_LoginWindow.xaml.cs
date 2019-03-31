6caaa27ad08c96e4053b7ebeec32d44b4b8656c3
Sami <Sami-esh@hotmail.com>
2019-03-31 14:15:12 +0300

    Finish Main Window Style
    - add title bar [ icon , Title , Buttons controls window ]

--- iMessenger/Signup_LoginWindow.xaml.cs
+++ iMessenger/Signup_LoginWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using iMessenger.Scripts;
 
 namespace iMessenger
 {
@@ -20,12 +21,30 @@ namespace iMessenger
     /// </summary>
     public partial class Signup_LoginWindow : Window
     {
+
         public Signup_LoginWindow()
         {
             InitializeComponent();
 
             this.DataContext = new WindowViewModel(this);
+
+            MainUser.SaveLocalMainUser(new MainUser("sami", "sami98", "s@gmail.com"));
+            MainUser mainUser = MainUser.LoadLocalMainUser();
+            #region /* My Code */
+
+            if (mainUser == null)
+            {
+                Console.WriteLine("No Local Main User !");
+            }
+            else
+            {
+                Console.WriteLine("Main User Founded !");
+                //TODO: leave this window and open iMessenger Window
+            }
+
+            #endregion
+
         }
-        
+
     }
 }
--- iMessenger/ViewModel/WindowViewModel.cs
+++ iMessenger/ViewModel/WindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace iMessenger
 {
@@ -29,12 +30,26 @@ namespace iMessenger
 
         #region Public Properties
 
+        /// <summary>
+        /// The smallest width of the window
+        /// </summary>
+        public double WindowMinimumWidth { get; set; } = 400;
+        /// <summary>
+        /// The smallest Height of the window
+        /// </summary>
+        public double WindowMinimumHeight { get; set; } = 400;
+
         public int ResizeBorder{ get; set; } = 6;
 
         /// <summary>
         /// The Size of the resize border around the window , taking into account   the outer margin
         /// </summary>
         public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+        
+        /// <summary>
+        /// The Size of Inner Content of main window
+        /// </summary>
+        public Thickness InnerContentPadding { get { return new Thickness(ResizeBorder); } }
 
         /// <summary>
         /// The Margin around the window to allow for a drop shadow
@@ -87,6 +102,15 @@ namespace iMessenger
         public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight+ResizeBorder); } }
         #endregion
 
+        #region Commands
+
+        public ICommand MinimizedCommand { get; set; }
+        public ICommand MaximizedCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+        public ICommand MenuCommand { get; set; }
+
+        #endregion
+
         #region Constractor
 
         /// <summary>
@@ -105,6 +129,14 @@ namespace iMessenger
                 OnPropertyChanged(nameof(WindowRadius));
                 OnPropertyChanged(nameof(WindowCornerRadius));
             };
+
+            //Create commands
+            MinimizedCommand = new RelayCommand(()=> mWindow.WindowState = WindowState.Minimized);
+            MaximizedCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand     = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(()=> SystemCommands.ShowSystemMenu(mWindow , new Point(0,0)) );
+
+            var resize = new WindowResizer(mWindow);
         }
 
         #endregion
