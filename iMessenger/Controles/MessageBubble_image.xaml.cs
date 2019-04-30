94b8d8ea4c2f1ce0d901768533d3746d241d8956
Sami <Sami-esh@hotmail.com>
2019-05-01 00:40:27 +0300

    Create Message Bubble UI

---
+++ iMessenger/Controles/MessageBubble_image.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace iMessenger
+{
+    /// <summary>
+    /// Interaction logic for MessageBubble.xaml
+    /// </summary>
+    public partial class MessageBubble_image : UserControl
+    {
+        public MessageBubble_image()
+        {
+            InitializeComponent();
+        }
+        public MessageBubble_image(string filePath , bool FriendMsgFlag)
+        {
+            InitializeComponent();
+            this.Image.Source = new BitmapImage(new Uri(filePath));
+            if (FriendMsgFlag) { this.BubbleBorder.Background = Brushes.Beige; this.HorizontalAlignment = HorizontalAlignment.Left; }
+            else { this.BubbleBorder.Background = Brushes.White; this.HorizontalAlignment = HorizontalAlignment.Right; }
+        }
+    }
+}
